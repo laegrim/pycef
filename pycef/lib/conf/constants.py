8ddddf80fcba500ad986ffb6c58aa857ef5d9a63
Samuel Teich <steich@gmail.com>
2013-08-17 14:42:43 -0500

    More of the Same

--- pycef/lib/conf/constants.py
+++ pycef/lib/conf/constants.py
@@ -11,6 +11,6 @@ CONF_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_CONF_LOC = os.path.dirname(os.path.abspath(__file__)) + '/logging.conf'
 SCRAPE_CONF_LOC = os.path.dirname(os.path.abspath(__file__)) + '/scrape.conf'
 
-print 'CONF_DIR'
-print 'LOG_CONF_LOC'
-print 'SCRAPE_CONF_LOC'
+print CONF_DIR
+print LOG_CONF_DIR
+print SCRAPE_CONF_LOC
