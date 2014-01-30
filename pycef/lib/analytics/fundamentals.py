57f54ce083187e5b1c1dd7a55e8188cb47ff6e93
Samuel Teich <steich@gmail.com>
2014-01-30 17:07:28 -0600

    Fixed stupid errors in EMA functions

--- pycef/lib/analytics/fundamentals.py
+++ pycef/lib/analytics/fundamentals.py
@@ -32,26 +32,89 @@ def best_fit_line_points(series):
     #return a series of points along that line
     return [((fit[0] * i) + fit[1]) for i in range(len(series))]
 
-def exp_mov_avg(series, duration, alpha = None):
+def exp_mov_avg(series, alpha = None, S1 = None):
     '''
-    Return the exponential moving average over a series
-    S(1) = Y(1)
-    for t > 1, S(t) = alpha * Y(t) + (1 - alpha) * S(t-1)
-    If an alpha is not given, alpha defaults to (2.0/(duration + 1))
+    -Series is an array of numerical values.
+    -Alpha is a constant weighting coeficient, and the 
+    higher alpha, the more biased the EXP will be towards
+    values in low indices. If no alpha is given, alpha defaults to 
+    (2/len(series) + 1).
+    -S1 is the value of the base case.  This value is always mathematically 
+    undefined, so there is always error introduced.  If no S1 is given, 
+    S1 defaults to series[0].
+
+    Returns the exponential moving average over a series
+    S(1) = S1
+    for t > 1, S(t) = alpha * Y(t - 1) + (1 - alpha) * S(t - 1)
     '''
-    
+
     #check to see if default alpha is used
     if alpha == None:
-        alpha = (2.0/(duration + 1))
+        alpha = (2.0/(len(series) + 1))
+
+    #check to see if the base case is defined
+    if S1 == None:
+        S1 == series[0]
+
+    #Base Case
+    if len(series) == 1:
         
-    #base case    
-    if duration == len(series):
-        return series[duration - 1]
-    
-    #recursively calculate the sum
+        #print statement is just interesting to watch sometimes
+        #print ('S(1) = ' + str(series[0]))    
+        return series[0]
+        
+    #t > 1
     else:
-        return ((alpha * series[duration]) + ((1 - alpha) * exp_mov_avg(series,
-                 duration - 1)))
+        
+        #print statement is just interesting to watch sometimes
+        #print ('S(' + str(len(series)) + ') = ' + \
+        #str(alpha) + ' * (' + str(series[len(series) - 2]) + \
+        #') + (' + str(1 - alpha) + ' + S(' + str(len(series) - 1) + '))')
+        
+        return ((alpha * series[len(series) - 2]) + \
+        (1 - alpha)*(alt_exp_mov_avg(series[:len(series) - 1], alpha)))
+
+def alt_exp_mov_avg(series, alpha = None, S1 = None):
+    '''
+    -Series is an array of numerical values.
+    -Alpha is a constant weighting coeficient, and the 
+    higher alpha, the more biased the EXP will be towards
+    values in low indices. If no alpha is given, 
+    alpha defaults to (2/len(series) + 1).
+    -S1 is the value of the base case.  This value is always mathematically 
+    undefined, so there is always error introduced.  If no S1 is given, 
+    S1 defaults to series[0].
+
+    Returns the exponential moving average over a series
+    S(0) = S(series[0]) = Y(series[0]) = Y(0), and
+    for t > 0, S(t) = alpha * Y(t) + (1 - alpha) * S(t - 1)
+    '''
+
+    #check to see if default alpha is used
+    if alpha == None:
+        alpha = (2.0/(len(series) + 1))
+
+    #check to see if the base case is defined
+    if S1 == None:
+        S1 == series[0]
+
+    #Base Case
+    if len(series) == 1:
+        
+        #print statement is just interesting to watch sometimes
+        #print ('S(1) = ' + str(series[0]))    
+        return series[0]
+        
+    #t > 1
+    else:
+        
+        #print statement is just interesting to watch sometimes
+        #print ('S(' + str(len(series)) + ') = ' + \
+        #str(alpha) + ' * (' + str(series[len(series) - 1]) + \
+        #') + (' + str(1 - alpha) + ' + S(' + str(len(series) - 1) + '))')
+        
+        return ((alpha * series[len(series) - 1]) + \
+        (1 - alpha)*(alt_exp_mov_avg(series[:len(series) - 1], alpha)))
 
 def simple_moving_avgs(series, duration):
     '''
@@ -75,14 +138,17 @@ def simple_moving_avgs(series, duration):
 
     return avgs
     
-def exp_mov_avgs(series, duration):
+def exp_mov_avgs(series, section_len, alpha = None, S1 = None):
     '''
-    Returns the exponential moving averages over a given duration for a given 
-    series of points
+    Returns a time series of exponential moving averages
+    
+    For a series of length N, returns [EMA(series[0:section_len]), 
+    EMA(series[1:section_len + 1]), ....]
+    
     '''
 
     #find the number of moving averages in the given series
-    num_moving_avgs = len(series) - duration + 1
+    num_moving_avgs = len(series) - section_len + 1
     avgs = []
 
     #ensure basic level of correctness
@@ -91,7 +157,7 @@ def exp_mov_avgs(series, duration):
     #calculate the moving averages using pre-defined exponential moving 
     #averages function on subseries
     for i in range(num_moving_avgs):
-        avgs.append(exp_mov_avg([series[i] for i in range(i, duration + i)], 
-                                 duration))
+        avgs.append(exp_mov_avg(series[i:section_len + i], alpha, S1))
     return avgs
+    
     
--- pycef/lib/mining/clique_analysis.py
+++ pycef/lib/mining/clique_analysis.py
@@ -4,9 +4,14 @@ Created on Thu Oct 10 09:36:56 2013
 
 @author: laegrim
 """
+
 import sys
+sys.path.append('/home/laegrim/pycef')
 from pycef.lib.mongo.mongo_interface import Mongo
 import heapq
+from networkx.readwrite import json_graph
+import networkx as nx
+import time
 
 def simple_enum_pseudo_clique(G,K,T,L):
     '''
@@ -31,22 +36,6 @@ def simple_enum_pseudo_clique(G,K,T,L):
     T: Pseudo clique denisty threshold as a float
     L: List of pseudo cliques
     '''
-    #Sort L so that duplication can be checked
-    #If the current pseudo clique is a duplicate
-    #We can exit early, else
-    #Append the most recent pesudo clique to L
-#    L.sort(cmp=sort_by_len)
-#    [clique.sort() for clique in L]
-#    nodes_list = K.nodes()
-#    nodes_list.sort()
-#    if L.count(nodes_list) > 0 or nodes_list in L > 0:
-#        print nodes_list
-#        return
-#    else:
-#        L.append(nodes_list)
-#        with Mongo() as interface:
-#            interface.push_to_mongo({'_id':'nodes_list', 'list':L},
-#                                    'NodesList', 'List', {'_id':'nodes_list'})
         
     L.append(K.nodes())
 
@@ -54,10 +43,6 @@ def simple_enum_pseudo_clique(G,K,T,L):
     for node in [vertex for vertex in G.nodes() if vertex not in K.nodes()]:
         #Add that node to K, (K' = K U {v})
         K.add_node(node)
-#        
-#        if K.nodes() in L:
-#            K.remove_node(node)
-#            continue
         
         #Add associated edges between recently added none and
         #nodes already in K
@@ -89,7 +74,7 @@ def simple_enum_pseudo_clique(G,K,T,L):
                 #print "K of length " + str(K.number_of_nodes()) +
                 #" has child with member: " + node
                 #Since K' is a child of K, then look for children of K
-                enum_pseudo_clique(G,K,T,L)
+                simple_enum_pseudo_clique(G,K,T,L)
         #K was not a pseudo clique or K was not a child
         K.remove_node(node)
         
@@ -117,34 +102,25 @@ def simple_enum_pseudo_clique_no_repeat(G,K,T,L):
     T: Pseudo clique denisty threshold as a float
     L: List of pseudo cliques
     '''
-    #Sort L so that duplication can be checked
-    #If the current pseudo clique is a duplicate
-    #We can exit early, else
-    #Append the most recent pesudo clique to L
+    
     L.sort(cmp=sort_by_len)
     [clique.sort() for clique in L]
     nodes_list = K.nodes()
     nodes_list.sort()
     if L.count(nodes_list) > 0 or nodes_list in L > 0:
-        print nodes_list
+        #print nodes_list
         return
     else:
         L.append(nodes_list)
-        with Mongo() as interface:
-            interface.push_to_mongo({'_id':'nodes_list', 'list':L},
-                                    'NodesList', 'List', {'_id':'nodes_list'})
+#        with Mongo() as interface:
+#            interface.push_to_mongo({'_id':'nodes_list', 'list':L},
+#                                    'NodesList', 'List', {'_id':'nodes_list'})
         
-    #L.append(nodes_list)
-
     #For each vertex in G that is not in K
     for node in [vertex for vertex in G.nodes() if vertex not in K.nodes()]:
         #Add that node to K, (K' = K U {v})
         K.add_node(node)
-#        
-#        if K.nodes() in L:
-#            K.remove_node(node)
-#            continue
-        
+
         #Add associated edges between recently added none and
         #nodes already in K
         for other_node in K.nodes():
@@ -175,21 +151,19 @@ def simple_enum_pseudo_clique_no_repeat(G,K,T,L):
                 #print "K of length " + str(K.number_of_nodes()) +
                 #" has child with member: " + node
                 #Since K' is a child of K, then look for children of K
-                enum_pseudo_clique(G,K,T,L)
+                simple_enum_pseudo_clique_no_repeat(G,K,T,L)
         #K was not a pseudo clique or K was not a child
         K.remove_node(node)
-        
-
+    
 def enum_pseudo_cliques(G,K,T,L):
     '''
     Container for a pseudo clique enumeration function with some optimization
     '''
     
-    sorted_G = [{'degree(k)':0, 'node':node) for node in G.nodes()]
-    sorted_G = heapq.heapify(sorted_G)
+    sorted_G = {}
+    for node in G.nodes():
+        sorted_G[node] = 0
     sorted_K = []
-    sorted_k = heapq.heapify(sorted_K)
-    find_pseudo_cliques(G,K,T,L,sorted_G,sorted_K)
 
     def find_pseudo_cliques(G,K,T,L,P,Q):
         '''
@@ -215,12 +189,9 @@ def enum_pseudo_cliques(G,K,T,L):
         L: List of pseudo cliques
         P: sorted list of nodes in G, ordered by <(k)
         Q: sorted list of first delta nodes in K, ordered by <(k), where delta
-        is the difference in number of nodes between G and K.  If the 
+        is the difference in number of nodes between G and K.  If delta
         is larger than K, then Q contains all elements of K.
         '''
-    
-        #Output K
-        L.append(K.nodes())
         
         #If the algorithm just started there won't be any nodes in K or 
         #Q.  In this case, for every node v in G: 1) K U {v} is a pseudo clique
@@ -228,7 +199,7 @@ def enum_pseudo_cliques(G,K,T,L):
         #when K is empty is a child clique
         
         #P and Q contain ordered lists using ordering <(k). Given that the 
-        #first element in both lists is the lement with the lowest order,
+        #first element in both lists is the element with the lowest order,
         #and that in order to be a chld node, v in G must meet 1) K U {v} is a 
         #psedo cliqe and 2) v <(k) v*(K), if the fist element of P has lower 
         #than the first element of Q, then no v in P can satisfy the 
@@ -237,27 +208,247 @@ def enum_pseudo_cliques(G,K,T,L):
         #v in P can satisfy the requirements to make K` a child either.  Using 
         #a heap to order P yeilds O(log|V|) search for elements v where 
         #K U {v} is a pseudo clique and v <(k) v*(K).
-        threshold_K = threshold(K,T)  
         
-        #if the first two are not met, there can be no children
-        if not order(P,P[0],Q[0]):
-            return
-        elif not heapq.nlargest(1, P)[0]['degree(k)'] >= threshold_K:
-            return
-        #find the first element that
-        else
-            
-        #P and Q contain ordered lists using ordering <(k). child elements can 
-        #be found by traversing the vertices in Q, which are ordered, and
-        #for each u in Q, looking at the neighbors of u.  In this way l(v,k), 
-        #where l(v,K) --> (the the first element in Q not adjacent to v: if
-        # all elements in Q are adjacent to v, l(v,k) is infinite), can be 
-        #computed for each v in G adjacent to at least on u in Q.  Since this
-        #operation takes O(delta ** 2) in the worst case, and O(|V|+|E|) in the
-        #best case, and if a v satisfies K U {v} is a pseudo clique and 
-        #v*(K) <(k) v, then all v where v <(k) l(v,k) satisfy K U {v} is a 
-        #child and all children may be found in O(min{delta ** 2, |V| + |E|}).
+        if density(K) < T:
+            print "not a pseudo clique...."
+        threshold_K = threshold(K, T)
+        L.append(K.nodes())
+        
+        #If there are no nodes in Q, start from the begining
+        if Q == []:
+            for node in G:
+                #add a node
+                K.add_node(node)
+                #append the node to Q and remove it from P
+                heapq.heappush(Q,{'node':node, 'degree(k)':0})
+                P.pop(node)
+                for neighbor_v in G[node]:
+                    P[neighbor_v] += 1
+                #start the recursion
+                find_pseudo_cliques(G,K,T,L,P,Q)
+                #when returning, put things back the way they were
+                P[node] = 0
+                for neighbor_v in G[node]:
+                    P[neighbor_v] = 0
+                Q.pop()
+                K.remove_node(node)
+                
+        else:
+            diff = min(len(Q), abs(len(P) - len(Q)))
+
+            #only need to do the first few
+            for i in range(diff):
+                #look at each neighbor of u in Q
+                for neighbor in [node for node in G[Q[i]['node']] if node in P]:
+                    #if K U {v} is a pseudo clique and v*(K) <(k) v and 
+                    #v <(k) l(v,K) then v K U {v} is a child
+                    n_deg = P[neighbor]
+                    min_deg = Q[0]['degree(k)']
+                    min_name = Q[0]['node']
+                    lesser = less(neighbor, G, Q)
+                    #nasty compound if to check for the above
+                    if ((min_deg < n_deg or (min_deg == n_deg and
+                        min_name <= neighbor)) and 
+                        n_deg >= threshold_K and 
+                        (n_deg < lesser['degree(k)'] or 
+                        (n_deg == lesser['degree(k)'] and 
+                        neighbor <= lesser['node']))):         
+                        #given that K U {v} is a child, then recurse
+                        #add v to k
+                        K.add_node(neighbor)
+                        #get all the appropriate edges
+                        for other_node in K:
+                            if G.has_edge(other_node, neighbor):
+                                K.add_edge(other_node, neighbor)
+                                
+                        #pop neighbor from P
+                        P.pop(neighbor)
+                        #update degree K for nodes in P
+                        for neighbor_v in G[neighbor]:
+                            if P.has_key(neighbor_v):
+                                P[neighbor_v] += 1
+                                
+                        #update degree Q for nodes in Q
+                        Q = [{'node':node, 'degree(k)':len(K[node])} for node
+                            in K]
+                            
+                        heapq.heapify(Q)
+                        
+                        #call new iter
+                        find_pseudo_cliques(G,K,T,L,P,Q)
+                        
+                        #replace values for P and Q
+                        P[neighbor] = degree_K(G,K,neighbor)
+                        for neighbor_v in G[neighbor]:
+                            if P.has_key(neighbor_v):
+                                P[neighbor_v] -= 1
+                                
+                        K.remove_node(neighbor)
+                        
+                        Q = [{'node':node, 'degree(k)':len(K[node])} for node
+                            in K]
+                            
+                        heapq.heapify(Q)
+                        
+    find_pseudo_cliques(G,K,T,L,sorted_G,sorted_K)
     
+def enum_pseudo_cliques_no_repeat(G,K,T,L):
+    '''
+    Container for a pseudo clique enumeration function with some optimization
+    '''
+    
+    sorted_G = {}
+    for node in G.nodes():
+        sorted_G[node] = 0
+    sorted_K = []
+
+    def find_pseudo_cliques_no_repeat(G,K,T,L,P,Q):
+        '''
+        Based on algorithm by Takeaki Uno, in his paper 
+        "An Efficient Algorithm for Enumerating Pseudo
+        Cliques"
+        
+        The paper describes an adjacency relation on pseudo cliques:
+        
+            Given any maximal pseudo clique, removing a vertex with degree
+            below or equal to the average vertex degree yeilds a child pseudo
+            clique.Thus all maximal pseudo cliques can be found by traversing
+            the tree implied by this relationship in reverse (i.e. by taking an
+            empty set, and adding minimum degree vertices to that set to see if
+            they form a pseudo clique).
+            
+            Optimized to find children of a given pseudo clique in O(V)
+        
+        Enumerates all pseudo cliques in a given graph:
+        G: Non-directed Graph (Networkx Graph Object)
+        K: Empty Non-directed Graph (Networkx Graph Object)
+        T: Pseudo clique denisty threshold as a float
+        L: List of pseudo cliques
+        P: sorted list of nodes in G, ordered by <(k)
+        Q: sorted list of first delta nodes in K, ordered by <(k), where delta
+        is the difference in number of nodes between G and K.  If delta
+        is larger than K, then Q contains all elements of K.
+        '''
+        
+        #If the algorithm just started there won't be any nodes in K or 
+        #Q.  In this case, for every node v in G: 1) K U {v} is a pseudo clique
+        #by definition and 2) v <(k) v*(K) by definition.  Thus every node in G
+        #when K is empty is a child clique
+        
+        #P and Q contain ordered lists using ordering <(k). Given that the 
+        #first element in both lists is the element with the lowest order,
+        #and that in order to be a chld node, v in G must meet 1) K U {v} is a 
+        #psedo cliqe and 2) v <(k) v*(K), if the fist element of P has lower 
+        #than the first element of Q, then no v in P can satisfy the 
+        #requirements to make K` = K U {v} a child of K. Additionally, if the
+        #last element of P does not satisfy deg(k)(v) > T(K), then no element 
+        #v in P can satisfy the requirements to make K` a child either.  Using 
+        #a heap to order P yeilds O(log|V|) search for elements v where 
+        #K U {v} is a pseudo clique and v <(k) v*(K).
+        
+        if density(K) < T:
+            print "not a pseudo clique...."
+        threshold_K = threshold(K, T)
+        L.sort(cmp=sort_by_len)
+        [clique.sort() for clique in L]
+        nodes_list = K.nodes()
+        nodes_list.sort()
+        if L.count(nodes_list) > 0 or nodes_list in L > 0:
+            #print nodes_list
+            return
+        else:
+            L.append(nodes_list)
+#            with Mongo() as interface:
+#                interface.push_to_mongo({'_id':'nodes_list', 'list':L},
+#                                    'NodesList', 'List', {'_id':'nodes_list'})        
+        #If there are no nodes in Q, start from the begining
+        if Q == []:
+            for node in G:
+                #add a node
+                K.add_node(node)
+                #append the node to Q and remove it from P
+                heapq.heappush(Q,{'node':node, 'degree(k)':0})
+                P.pop(node)
+                for neighbor_v in G[node]:
+                    P[neighbor_v] += 1
+                #start the recursion
+                find_pseudo_cliques_no_repeat(G,K,T,L,P,Q)
+                #when returning, put things back the way they were
+                P[node] = 0
+                for neighbor_v in G[node]:
+                    P[neighbor_v] = 0
+                Q.pop()
+                K.remove_node(node)
+                
+        else:
+            diff = min(len(Q), abs(len(P) - len(Q)))
+
+            #only need to do the first few
+            for i in range(diff):
+                #look at each neighbor of u in Q
+                for neighbor in [node for node in G[Q[i]['node']] if node in P]:
+                    #if K U {v} is a pseudo clique and v*(K) <(k) v and 
+                    #v <(k) l(v,K) then v K U {v} is a child
+                    n_deg = P[neighbor]
+                    min_deg = Q[0]['degree(k)']
+                    min_name = Q[0]['node']
+                    lesser = less(neighbor, G, Q)
+                    #nasty compound if to check for the above
+                    if ((min_deg < n_deg or (min_deg == n_deg and
+                        min_name <= neighbor)) and 
+                        n_deg >= threshold_K and 
+                        (n_deg < lesser['degree(k)'] or 
+                        (n_deg == lesser['degree(k)'] and 
+                        neighbor <= lesser['node']))):         
+                        #given that K U {v} is a child, then recurse
+                        #add v to k
+                        K.add_node(neighbor)
+                        #get all the appropriate edges
+                        for other_node in K:
+                            if G.has_edge(other_node, neighbor):
+                                K.add_edge(other_node, neighbor)
+                                
+                        #pop neighbor from P
+                        P.pop(neighbor)
+                        #update degree K for nodes in P
+                        for neighbor_v in G[neighbor]:
+                            if P.has_key(neighbor_v):
+                                P[neighbor_v] += 1
+                                
+                        #update degree Q for nodes in Q
+                        Q = [{'node':node, 'degree(k)':len(K[node])} for node
+                            in K]
+                            
+                        heapq.heapify(Q)
+                        
+                        #call new iter
+                        find_pseudo_cliques_no_repeat(G,K,T,L,P,Q)
+                        
+                        #replace values for P and Q
+                        P[neighbor] = degree_K(G,K,neighbor)
+                        for neighbor_v in G[neighbor]:
+                            if P.has_key(neighbor_v):
+                                P[neighbor_v] -= 1
+                                
+                        K.remove_node(neighbor)
+                        
+                        Q = [{'node':node, 'degree(k)':len(K[node])} for node
+                            in K]
+                            
+                        heapq.heapify(Q)
+                        
+    find_pseudo_cliques_no_repeat(G,K,T,L,sorted_G,sorted_K)
+    
+def less(v, G, Q):
+    '''
+    Returns the node of least order in graph K not adjacent to v.  If all nodes
+    in K are adjacent to v, then returns high value.
+    '''
+    
+    for node in Q:
+        if not G.has_edge(v, node['node']):
+            return node
+    return {'node':'temp', 'degree(k)':1000000000}
     
 def degree_K(G,K,v):
     '''
@@ -265,7 +456,7 @@ def degree_K(G,K,v):
     in K
     '''
     i = 0
-    neighbors = G.neighbors(v)
+    neighbors = G[v]
     for neighbor in neighbors:
         if neighbor in K:
             i += 1
@@ -348,3 +539,53 @@ def find_interesting_keywords(graph, cliques_list, pageranks, keyword_list,
                 != [] ] != []: 
                 interesting_keywords.append((keyword, page[1]))
                 print keyword[0] + ', ' + keyword[1] + ', ' + keyword[3]
+                
+G = json_graph.load(open('/home/laegrim/pycef/pycef/visualizations/exp.json'))
+K = nx.Graph()
+T = .9
+
+L = []
+total = 0
+for i in range(6):
+    
+    
+    L = []
+
+    start = time.time()
+    simple_enum_pseudo_clique_no_repeat(G,K,T,L)
+    end = time.time()
+    total += end - start
+    print "finished"
+
+print "Average Time: " + str(total/6.0)
+
+total = 0
+M = []
+for i in range(6):
+    
+    M = []
+    start = time.time()
+    enum_pseudo_cliques(G,K,T,M)
+    end = time.time()
+    total += end - start
+    
+    print "finished"
+
+print "Average Time: " + str(total/6.0)
+
+
+total = 0
+N = []
+for i in range(6):
+    
+    N = []
+
+    start = time.time()
+    enum_pseudo_cliques_no_repeat(G,K,T,N)
+    end = time.time()
+    total += end - start
+    print "finished"
+
+print "Average Time: " + str(total/6.0)
+
+print "finished"
